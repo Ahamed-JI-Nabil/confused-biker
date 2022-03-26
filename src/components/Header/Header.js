a870180af5079f6432c896fd11626a166317f29c
AhamedJINabil <johayeriqbal@gmail.com>
2022-03-27 04:14:00 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Confused Biker</h1>
+            <h3>Your Options Are</h3>
+        </div>
+    );
+};
+
+export default Header;
