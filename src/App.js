27a599630fe3d59c1ab68fcc86a73da6e447f6a1
AhamedJINabil <johayeriqbal@gmail.com>
2022-03-27 04:28:09 +0600

    data added and single bike displayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Cards from './components/Cards/Cards';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Cards></Cards>
     </div>
   );
 }
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+import './Cards.css'
 
 const Cards = () => {
+
+    const [bikes, setBikes] = useState([])
+
+    useEffect(() => {
+        fetch('bikes.json')
+        .then(res => res.json())
+        .then(data => setBikes(data))
+    },[])
+
     return (
-        <div>
-            
+        <div className="cards">
+            {
+                bikes.map(bike => <Card key={bike.id} bike={bike}></Card>)
+            }
         </div>
     );
 };
